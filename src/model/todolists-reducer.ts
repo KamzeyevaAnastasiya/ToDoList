a33c0fff59adbe2d35e9c0b9db4ba98ba2890194
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-10-09 15:12:49 +0300

    Add reducer and test to change todolistTitle

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -3,7 +3,7 @@ import {v1} from "uuid";
 
 const initialState: Todolist[] = []
 
-type Actions = DeleteTodolistAction | CreateTodolistAction
+type Actions = DeleteTodolistAction | CreateTodolistAction | ChangeTodolistTitleAction
 
 export const deleteTodolistAC = (id: string) => {
     return {type: 'todolist/deleteTodolist', payload: { id }} as const
@@ -17,6 +17,12 @@ export const createTodolistAC = (title: string) => {
 
 export type CreateTodolistAction = ReturnType<typeof createTodolistAC>
 
+export const changeTodolistTitleAC = ({id, title}: {id: string, title: string}) => {
+    return {type: 'todolist/changeTodolistTitle', payload: {  id, title }} as const
+}
+
+export type ChangeTodolistTitleAction = ReturnType<typeof changeTodolistTitleAC>
+
 
 export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
     switch (action.type) {
@@ -27,6 +33,10 @@ export const todolistsReducer = (state: Todolist[] = initialState, action: Actio
             const newTodolist: Todolist = {id: action.payload.id, title: action.payload.title, filter: 'All'}
             return [...state, newTodolist]
         }
+        case 'todolist/changeTodolistTitle': {
+            return state.map(todolist => action.payload.id === todolist.id ? {...todolist, title: action.payload.title} : todolist)
+        }
+
         default:
             return state
     }
