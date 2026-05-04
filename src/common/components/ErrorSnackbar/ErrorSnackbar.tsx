7f878f9c6f3199769dec9ee0d299bf2a1b2db56a
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-04 18:04:07 +0300

    Add global state to error

--- src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,22 +1,26 @@
-import { SyntheticEvent, useState } from 'react'
+import { selectError, setAppErrorAC } from '@/app/app-slice'
+import { useAppDispatch, useAppSelector } from '@/common/hooks'
+import { SyntheticEvent } from 'react'
 import Alert from '@mui/material/Alert'
 import Snackbar from '@mui/material/Snackbar'
 
 export const ErrorSnackbar = () => {
-  const [open, setOpen] = useState(true)
+  const error = useAppSelector(selectError)
+
+  const dispatch = useAppDispatch()
 
   const handleClose = (_: SyntheticEvent | Event, reason?: string) => {
     if (reason === 'clickaway') {
       return
     }
 
-    setOpen(false)
+    dispatch(setAppErrorAC({ error: null }))
   }
 
   return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+    <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
       <Alert onClose={handleClose} severity="error" variant="filled" sx={{ width: '100%' }}>
-        Error message
+        {error}
       </Alert>
     </Snackbar>
   )
