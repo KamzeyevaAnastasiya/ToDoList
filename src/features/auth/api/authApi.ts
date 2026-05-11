134c5fb55debc048ff9d69bf8be67897a5e0498d
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-11 21:29:25 +0300

    Add me api

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,13 +1,16 @@
 import { instance } from '@/common/instance'
 import type { DefaultBaseResponse } from '@/common/types'
-import type { DefaultLoginResponse } from '@/features/auth/api/authApi.types'
+import type { LoginResponse, MeResponse } from '@/features/auth/api/authApi.types'
 import type { LoginInputs } from '@/features/auth/lib/schemas'
 
 export const authApi = {
   login(payload: LoginInputs) {
-    return instance.post<DefaultLoginResponse>('/auth/login', payload)
+    return instance.post<LoginResponse>('/auth/login', payload)
   },
   logout() {
     return instance.delete<DefaultBaseResponse>('/auth/login')
+  },
+  me() {
+    return instance.get<MeResponse>('/auth/me')
   },
 }
