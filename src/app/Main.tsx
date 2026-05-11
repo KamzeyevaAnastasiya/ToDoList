6d107bced670518e0d700771279ec13aa64afa38
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-11 22:57:17 +0300

    Move redirect to login to protected route

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,24 +1,15 @@
-import { useAppDispatch, useAppSelector } from '@/common/hooks'
-import { Path } from '@/common/routing'
-import { selectIsLoggedIn } from '@/features/auth/model/auth-slice'
+import { useAppDispatch } from '@/common/hooks'
 import { createTodolistTC } from '@/features/todolists/model/todolists-slice'
 import Grid from '@mui/material/Grid'
 import { CreateItemForm } from '@/common/components/CreateItemForm/CreateItemForm.tsx'
 import Container from '@mui/material/Container'
 import { Todolists } from '@/features/todolists/ui/Todolists/Todolists.tsx'
-import { Navigate } from 'react-router'
 
 export const Main = () => {
   const dispatch = useAppDispatch()
 
-  const isLoggedIn = useAppSelector(selectIsLoggedIn)
-
   const createTodolist = (title: string) => {
     dispatch(createTodolistTC(title))
-  }
-
-  if (!isLoggedIn) {
-    return <Navigate to={Path.Login} />
   }
 
   return (
