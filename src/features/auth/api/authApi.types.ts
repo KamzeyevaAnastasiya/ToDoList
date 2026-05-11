ed8c7874cfe6bedc7d1b8ff53fa7d17cbc57dc73
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-11 21:30:24 +0300

    Add schema to me api and refactoring

--- src/features/auth/api/authApi.types.ts
+++ src/features/auth/api/authApi.types.ts
@@ -5,6 +5,17 @@ export const loginSchema = z.object({
   userId: z.number(),
   token: z.string(),
 })
-export const defaultLoginResponseSchema = baseResponseSchema(loginSchema)
 
-export type DefaultLoginResponse = z.infer<typeof defaultLoginResponseSchema>
+export const loginResponseSchema = baseResponseSchema(loginSchema)
+
+export type LoginResponse = z.infer<typeof loginResponseSchema>
+
+export const meSchema = z.object({
+  id: z.number(),
+  email: z.string(),
+  login: z.string(),
+})
+
+export const meResponseSchema = baseResponseSchema(meSchema)
+
+export type MeResponse = z.infer<typeof meResponseSchema>
