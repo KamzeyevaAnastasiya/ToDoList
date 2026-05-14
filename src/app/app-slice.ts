2ac37041d063be68035bfa86c5bfa688d90fe080
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-14 22:01:24 +0300

    Add matcher to loader

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,5 +1,5 @@
 import type { RequestStatus } from '@/common/types'
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, isFulfilled, isPending, isRejected } from '@reduxjs/toolkit'
 
 export const appSlice = createSlice({
   name: 'app',
@@ -14,6 +14,18 @@ export const appSlice = createSlice({
     selectStatus: (state) => state.status,
     selectError: (state) => state.error,
     selectIsLoggedIn: (state) => state.isLoggedIn,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(isPending, (state) => {
+        state.status = 'loading'
+      })
+      .addMatcher(isFulfilled, (state) => {
+        state.status = 'succeeded'
+      })
+      .addMatcher(isRejected, (state) => {
+        state.status = 'failed'
+      })
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
