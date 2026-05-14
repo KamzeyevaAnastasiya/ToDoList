7a49bdb0292cdd9a4fafc5960c6c2c1299f61c4f
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-14 22:07:34 +0300

    Add skeleton for todolist

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,10 +1,25 @@
+import { containerSx } from '@/common/styles'
 import { useGetTodolistsQuery } from '@/features/todolists/api/todolistsApi'
+import { TodolistSkeleton } from '@/features/todolists/ui/Todolists/TodolistSkeleton/TodolistSkeleton'
 import { Paper } from '@mui/material'
+import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import { TodolistItem } from '@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx'
 
 export const Todolists = () => {
-  const { data: todolists } = useGetTodolistsQuery()
+  const { data: todolists, isLoading } = useGetTodolistsQuery()
+
+  if (isLoading) {
+    return (
+      <Box sx={containerSx} style={{ gap: '32px' }}>
+        {Array(3)
+          .fill(null)
+          .map((_, id) => (
+            <TodolistSkeleton key={id} />
+          ))}
+      </Box>
+    )
+  }
 
   return (
     <>
