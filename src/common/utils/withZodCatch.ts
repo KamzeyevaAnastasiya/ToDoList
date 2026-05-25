2f66c2a68fa7ea1d8bd4868b94d31ba9a8aca77a
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-05-26 02:56:00 +0300

    Refactoring

--- src/common/utils/withZodCatch.ts
+++ src/common/utils/withZodCatch.ts
@@ -1,5 +1,3 @@
-import { setAppErrorAC } from '@/app/app-slice'
-import { store } from '@/app/store'
 import type { NamedSchemaError } from '@reduxjs/toolkit/query'
 import type { FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
 import type { ZodType } from 'zod'
@@ -7,7 +5,6 @@ import type { ZodType } from 'zod'
 export const withZodCatch = <T extends ZodType>(schema: T) => ({
   responseSchema: schema,
   catchSchemaFailure: (err: NamedSchemaError): FetchBaseQueryError => {
-    store.dispatch(setAppErrorAC({ error: 'Zod error. Details in the console' }))
     console.error('Zod error. Details in the console', err.issues)
     return { status: 'CUSTOM_ERROR', error: 'Schema validation failed' }
   },
